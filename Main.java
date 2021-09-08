50d36426491d16895a09788fa9d23e753954f9d2
Michael Gilday <>
2021-09-08 01:14:12 +0000

    As 1-4

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
  *  Copyright 2021 Michael Gilday
  */
 
-import java.util.Scanner;
+import java.util.*;
 
 //Exercise 1 - Saying Hello. (Prompts for your name and prints a greeting using your name.)
 class Main {
@@ -11,7 +11,7 @@ class Main {
     Scanner start_scan = new Scanner(System.in);  //Creation of a scanner object to allow for user input.
 
     System.out.println("What is your name? ");
-    String Name = start_scan.nextLine();  //Used to read in the name.
+    String Name = start_scan.next();  //Used to read in the name.
     System.out.println("Hello, " + Name +", nice to meet you!");  //Prints out statement and the input name.
     start_scan.close();
   }
