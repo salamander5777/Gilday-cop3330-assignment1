ed48c0298b7663d3c0450e9d4a4e1fe17f81d4df
Michael Gilday <>
2021-09-10 16:36:02 +0000

    As 1-19 nice

--- Main.java
+++ Main.java
@@ -5,32 +5,17 @@
 
 import java.util.*;
 
-//Exercise 19 - BMI Calculator. (Program that calculates the body mass index (BMI) for a person using the person’s height in inches and weight in pounds, which is prompted by the program.)
+//Exercise 16 - Legal Driving Age. (Program that prompts the user for their age and compares it to the legal driving age of 16. Will output whether or not you are legally allowed to drive according to this.)
 class Main {
   public static void main(String[] args) {
     Scanner start_scan = new Scanner(System.in);  //Creation of a scanner object to allow for user input.
 
-    System.out.print("What is your height in inches? ");
-    String input_H = start_scan.next();  //Reads in height.
-    if (!input_H.matches("[0.0-9.0]+")) {
-      System.out.println("Error: Input a valid positive number.");
-      System.exit(0);
-    }
+    System.out.print("What is your age? ");
+    String input_age = start_scan.next();  //Reads in the age.
+    int age = Integer.parseInt(input_age);
 
-    System.out.print("What is your weight in pounds? ");
-    String input_W = start_scan.next();  //Reads in pounds.
-    if (!input_W.matches("[0.0-9.0]+")) {
-      System.out.println("Error: Input a valid positive number.");
-      System.exit(0);
-    }
 
-    //Below is the block of code used to convert input strings into doubles and do the math. BMI is calculated as: (Weight/(Height*Height))*703.
-    double H = Double.parseDouble(input_H);
-    double W = Double.parseDouble(input_W);
-    double BMI = (W/(H*H))*703.0;
-    BMI = Math.round(BMI*10.00)/10.00;
-
-    System.out.print( (BMI >= 18.5 && BMI < 25.0) ? "Your BMI is " + BMI + ".\n" + "You are within the ideal weight range." : (BMI > 25.0) ? "Your BMI is " + BMI + ".\n" + "You are overweight. You should see your doctor." : "Your BMI is " + BMI + ".\n" + "You are underweight. You should see your doctor.");  //Prints out your current BMI and whether or not you are within ideal weight range.
+    System.out.print( (age >= 16) ? "You are old enough to legally drive." : "You are not old enough to legally drive.");  //Prints out whether you are legally allowed to drive in FL or not based solely on age.
 
     start_scan.close();
   }
