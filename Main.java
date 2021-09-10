812154513884966284c2ab10d7cd8075b80436ac
Michael Gilday <>
2021-09-10 17:22:59 +0000

    As 1-20

--- Main.java
+++ Main.java
@@ -5,18 +5,17 @@
 
 import java.util.*;
 
-//Exercise 16 - Legal Driving Age. (Program that prompts the user for their age and compares it to the legal driving age of 16. Will output whether or not you are legally allowed to drive according to this.)
+//Exercise 3 - Printing Quotes. (Program that prompts for a quote and an author. Then displays the quotation and author as shown in the example output.)
 class Main {
   public static void main(String[] args) {
     Scanner start_scan = new Scanner(System.in);  //Creation of a scanner object to allow for user input.
 
-    System.out.print("What is your age? ");
-    String input_age = start_scan.next();  //Reads in the age.
-    int age = Integer.parseInt(input_age);
-
-
-    System.out.print( (age >= 16) ? "You are old enough to legally drive." : "You are not old enough to legally drive.");  //Prints out whether you are legally allowed to drive in FL or not based solely on age.
-
+    System.out.print("What is the quote? ");
+    String Quote = start_scan.nextLine();  //Reads in the quote.
+    System.out.print("Who said it? ");
+    String Author = start_scan.nextLine(); //Reads in 'who said the quote'.
+    
+    System.out.print(Author + " says, " + "\"" + Quote + "\"");  //Prints out who said the quote along with the quote itself.
     start_scan.close();
   }
 }
